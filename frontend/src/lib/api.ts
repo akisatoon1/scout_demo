8e4e6f6ba1f64f676e7bc75916dfce383428f8a7
akisatoon <akisatoon@gmail.com>
2025-04-12 03:45:12 +0900

    modify port number

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const API_BASE_URL = 'http://localhost:3000/api';
+
 const api = axios.create({
-    baseURL: 'http://localhost:3001/api',
+    baseURL: API_BASE_URL,
     withCredentials: true,
 });
 
